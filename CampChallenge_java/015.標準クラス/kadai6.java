bae27fb59f6f3977c7a82b9234844265d1944066
ErikoTadama <erikotadama32@gmail.com>
2018-02-23 11:59:17 +0900

    標準クラス文字列編課題２提出します。

---
+++ CampChallenge_java/015.標準クラス/kadai6.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+/**
+ *
+ * @author eriko
+ */
+public class kadai6 {
+    public static void main(String[] args) {
+        String ad = "erikotadama32@gmail.com";
+
+        
+        System.out.println(ad.substring(13));
+
+                System.out.println(ad.substring(2, 5));
+    }
+    
+}

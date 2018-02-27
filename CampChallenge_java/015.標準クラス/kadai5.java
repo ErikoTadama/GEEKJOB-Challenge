2470f20c0e670305a887373615d99dc5538adeee
ErikoTadama <erikotadama32@gmail.com>
2018-02-27 17:53:30 +0900

    標準クラス文字列編課題１提出します。

---
+++ CampChallenge_java/015.標準クラス/kadai5.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.sample;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author eriko
+ */
+public class kadai5 {
+
+    public static void main(String[] args) {
+        String word = "田玉えりこ";
+
+        System.out.println(word.length());
+        System.out.println(Arrays.toString(word.getBytes()));
+        
+    }
+}
